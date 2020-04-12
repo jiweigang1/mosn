76c2dec7affd2ab373dcd9ba1165c81e63f87517
冀刚 <jiweigang2008@126.com>
2020-04-12 18:29:10 +0800

    注释
    注释

--- pkg/protocol/xprotocol/header.go
+++ pkg/protocol/xprotocol/header.go
@@ -23,20 +23,23 @@ import (
 )
 
 // BytesKV key-value pair in byte slice
+// key value Map key 其实是String 使用 byte 方式存储，可以减少后续的 String 字节转换处理 
 type BytesKV struct {
 	Key   []byte
 	Value []byte
 }
 
 // Header consists of multi key-value pair in byte slice formation. This could reduce the cost of []byte to string for protocol codec.
+// 通用的 Header 结构
 type Header struct {
 	Kvs []BytesKV
-
-	Changed bool
+	Changed bool // Header 是否发生变化，设置了Header的值就表示为true 标示发生了变化
 }
 
 // ~ HeaderMap
+// 获取 Header 的值 Value 也是String
 func (h *Header) Get(Key string) (Value string, ok bool) {
+	//遍历所有的 KV 获取匹配的值
 	for i, n := 0, len(h.Kvs); i < n; i++ {
 		kv := &h.Kvs[i]
 		if Key == string(kv.Key) {
@@ -45,18 +48,20 @@ func (h *Header) Get(Key string) (Value string, ok bool) {
 	}
 	return "", false
 }
-
+// 设置值值
 func (h *Header) Set(Key string, Value string) {
+	//标示Header 发生了变化
 	h.Changed = true
-
+	//遍历所有的KV，
 	for i, n := 0, len(h.Kvs); i < n; i++ {
 		kv := &h.Kvs[i]
+		//如果是已经存在，追加到值的内容中
 		if Key == string(kv.Key) {
 			kv.Value = append(kv.Value[:0], Value...)
 			return
 		}
 	}
-
+	// 如果不存在添加新的KV进行存储
 	var kv *BytesKV
 	h.Kvs, kv = allocKV(h.Kvs)
 	kv.Key = append(kv.Key[:0], Key...)
@@ -117,11 +122,13 @@ func (h *Header) ByteSize() (size uint64) {
 	}
 	return
 }
-
+//申请新的KV结构
 func allocKV(h []BytesKV) ([]BytesKV, *BytesKV) {
 	n := len(h)
+	// 如果数组容量够用，直接在添加在末尾
 	if cap(h) > n {
 		h = h[:n+1]
+	//扩充数组	
 	} else {
 		h = append(h, BytesKV{})
 	}
