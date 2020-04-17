98ffd97bfad27347384431160e1b991ce17833d4
冀刚 <jiweigang2008@126.com>
2020-04-17 09:18:49 +0800

    注释
    注释

--- pkg/stream/factory.go
+++ pkg/stream/factory.go
@@ -23,13 +23,13 @@ import (
 	"mosn.io/api"
 	"mosn.io/mosn/pkg/types"
 )
-
+//各种协议解析的工场映射，key 协议的名称，value 协议得解析方式
 var streamFactories map[types.ProtocolName]ProtocolStreamFactory
 
 func init() {
 	streamFactories = make(map[types.ProtocolName]ProtocolStreamFactory)
 }
-
+//注册协议注册的工场
 func Register(prot types.ProtocolName, factory ProtocolStreamFactory) {
 	streamFactories[prot] = factory
 }
@@ -43,14 +43,19 @@ func CreateServerStreamConnection(context context.Context, prot api.Protocol, co
 
 	return nil
 }
-
+//根据协议工场的匹配函数选择协议解析的工场
 func SelectStreamFactoryProtocol(ctx context.Context, prot string, peek []byte) (types.ProtocolName, error) {
 	var err error
 	var again bool
+	//遍历所有的协议解析工厂的匹配函数进行匹配，来选择协议解析工场
 	for p, factory := range streamFactories {
+		//匹配协议解析函数
 		err = factory.ProtocolMatch(ctx, prot, peek)
+		//如果返回nil表示匹配成功
 		if err == nil {
+			//返回匹配的工场
 			return p, nil
+			//如果返回 eagain，证明读取的字节长度不够，需要重新匹配
 		} else if err == EAGAIN {
 			again = true
 		}
